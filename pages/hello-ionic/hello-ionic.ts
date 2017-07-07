c15ba535ef2bc261ee211eb44b2f4539544241fc
phat.ngo.inter <phat.ngo.inter>
2017-07-07 11:47:35 +0700

    a

---
+++ pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { YoutubeVideoPlayer } from 'ionic-native';
+
+
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+  constructor() {
+  	
+  }
+
+}
